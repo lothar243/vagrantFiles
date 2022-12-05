1e4f15b3f900df6e16ff38e60f1d71f0f463a871
Jeff Arends <jeffarends@gmail.com>
2022-12-05 09:54:21 -0700

    Split the provisioning to its own file, and change from Ubuntu Server to Centos/7

---
+++ linuxServer1/provision.sh
@@ -0,0 +1,7 @@
+sudo yum -y update
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+sudo systemctl enable docker
+sudo systemctl start docker
